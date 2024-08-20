c4eebee9cdf3f497c8ec1fbc3ebf1401fb76583b
Ben Fitzhardinge <fitzbe@MacBook-Pro-2.local>
2024-08-20 23:30:49 +0800

    Feedback for code

--- 04_parentpetclass.py
+++ 04_parentpetclass.py
@@ -1,5 +1,8 @@
 # Parent Pet Class
 class Pet:
+
+    noise = "Generic pet noise"
+
     def __init__(self, name, breed, age, fur_colour):
         self.name = name
         self.breed = breed
@@ -15,40 +18,31 @@ class Pet:
     def petting(self):
         print(f"{self.name} is being petted.")
 
-    def noise(self):
-        print("Generic pet noise")
+    def speak(self):
+        print(self.noise)
 
 
 class Dog(Pet):
-    def noise(self):
-        return "Woof!"
-
-class Cat(Pet):
-    def noise(self):
-        print("Meow!")
+    noise = "Woof!"
 
 
-# Dogs
-# Instantiating the dog class, to create unique instances i.e. Dog1 and Dog2
+# Inherited Dogs
+# Instantiating the dog class, to create unique instances i.e. my_dog and my_other_dog
 Dog1 = Dog("Ruby", "", 2, "Brown")
 Dog2 = Dog("Happy", "Golden Retriever", 3, "White")
 
-print(Dog2.noise())
+print(Dog2.speak())
 Dog1.eat()
 
-# Cats
+
+# Polymophic Power Cats
+class Cat(Pet):
+    noise = "Meow!"
+
+
 # Instantiating the cat class, to create unique instances i.e. Cat1 and Cat2
 Cat1 = Cat("Whiskers", "British Shorthair", 4, "Blue")
 Cat2 = Cat("Sylvester", "Tabby", 5, "Orange")
 
 Cat1.sleep()
-Cat2.noise()
-
-
-
-
-
-
-
-
-
+Cat2.speak()
